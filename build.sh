f370dfb5d67f64acc120d5a059f5cc6773ae44e4
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-07 16:21:34 +0300

    Teste de django

--- build.sh
+++ build.sh
@@ -3,10 +3,12 @@
 #exit on error
 set -o errexit
 
-#poetry install
-#poetry self update
-#rm poetry.lock
-#poetry lock
+pip freeze
+
+poetry install
+poetry self update
+rm poetry.lock
+poetry lock
 
 python manage.py collectstatic --no-input
 python manage.py migrate
