cc1ab5177870c9626f7dbafe7d717a94820b443b
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-07 16:18:02 +0300

    Recutification de build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 #exit on error
 set -o errexit
 
-poetry install
-poetry self update
-rm poetry.lock
-poetry lock
+#poetry install
+#poetry self update
+#rm poetry.lock
+#poetry lock
 
 python manage.py collectstatic --no-input
 python manage.py migrate
