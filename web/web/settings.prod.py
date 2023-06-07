e3fe975dd60c2d839ca1b96b72b83719576aab61
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-07 14:50:08 +0300

    Configuration de django pour postgresql : poetry add dj-database-url psycopg2-binary et configuration de settings.prod.py

--- web/web/settings.prod.py
+++ web/web/settings.prod.py
@@ -1,12 +1,21 @@
-from .settings import *
 import os
+import dj_database_url
+from .settings import *
 
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-d4c*s#i%@!&k+p$svf3(pq9yr2kf20#0g)4u@0h26wh861r#9l')
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', default='django-insecure-d4c*s#i%@!&k+p$svf3(pq9yr2kf20#0g)4u@0h26wh861r#9l')
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME') 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
-if RENDER_EXTERNAL_HOSTNAME:    
+if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
+}
