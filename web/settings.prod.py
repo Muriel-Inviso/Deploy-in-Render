9dd38930d60cd1d1fbbc730ae724ab40a98960de
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-08 09:56:30 +0300

    Manipuler de setting.prod

--- web/settings.prod.py
+++ web/settings.prod.py
@@ -2,14 +2,14 @@ import dj_database_url
 from .settings import *
 
 SECRET_KEY = os.environ.get(
-    'SECRET_KEY', default='django-insecure-d4c*s#i%@!&k+p$svf3(pq9yr2kf20#0g)4u@0h26wh861r#9l')
+    'SECRET_KEY', default='1ef68649e6429d5074d7138346477ef2')
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
-if RENDER_EXTERNAL_HOSTNAME:
+if RENDER_EXTERNAL_HOSTNAME:    
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 DATABASES = {
