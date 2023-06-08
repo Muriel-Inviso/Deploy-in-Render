1734cf5778757220ea57565b9e116369ebb2f8b2
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-08 14:04:59 +0300

    Test 2

--- web/settings_prod.py
+++ web/settings_prod.py
@@ -8,7 +8,7 @@ DEBUG = 'RENDER' not in os.environ
 
 print(DEBUG)
 
-ALLOWED_HOSTS = ['web-deploy-41de.onrender.com']
+ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
@@ -31,12 +31,18 @@ STATIC_URL = '/static/'
 if not DEBUG:
     print(f"not DEBUG: {DEBUG}")
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Dites à Django de copier les statiques dans le répertoire `staticfiles`
     # dans votre répertoire d'application sur Render.
     # Activez le backend de stockage WhiteNoise qui prend en charge la compression des fichiers statiques
     # et en créant des noms uniques pour chaque version afin qu'ils puissent être mis en cache en toute sécurité pour toujours.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 
+    STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 else: 
     print(f"DEBUG: {DEBUG}")
