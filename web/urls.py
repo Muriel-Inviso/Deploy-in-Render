04a4002e3d90b4b7b0b6f153ed703dd79331ad72
muriel.raharison <muriel.raharison@inviso-group.com>
2023-06-08 13:13:39 +0300

    Teste erreur 2

--- web/urls.py
+++ web/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('render.urls')),
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
